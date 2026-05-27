1d5519150ec106eec17c865ee0cb8c9074984d34
Lívia Andrade <livia.aandrade2006@gmail.com>
2026-05-26 22:32:34 -0300

    Segunda Atividade

---
+++ DesafioDojo/Biblioteca.java
@@ -0,0 +1,10 @@
+public class Biblioteca {
+    public String nomeBiblioteca;
+    public int quantidadeLivros;
+    public static int contabilizarLivros;
+
+
+    public static void exibirTotalLivros(){
+        System.out.println("Total: " + contabilizarLivros);
+    }
+}
